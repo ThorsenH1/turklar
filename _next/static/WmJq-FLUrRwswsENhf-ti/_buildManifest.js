f423e34363dbc9e7dd2c3a9873101288350de902
Halvor Thorsen <din_epost@example.com>
2026-03-23 13:26:54 +0100

    Updates

--- _next/static/WmJq-FLUrRwswsENhf-ti/_buildManifest.js
+++ _next/static/WmJq-FLUrRwswsENhf-ti/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/turklar//_next/:path+",
+        "destination": "/turklar/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
